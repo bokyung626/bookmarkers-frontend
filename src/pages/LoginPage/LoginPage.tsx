b10bb3622ac617c2b349cba0eabbfd8066d4e092
bokyung626 <ruddl033@naver.com>
2024-07-03 23:54:16 +0900

    fix: Update LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -4,10 +4,13 @@ import * as S from "./style";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
+import { useUserData } from "../../hooks/useUserData";
+import { useDispatch } from "react-redux";
+import { login } from "../../redux/actions";
 
 export const LoginPage: React.FC = () => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
@@ -29,11 +32,16 @@ export const LoginPage: React.FC = () => {
       await axios
         .post("/auth/login", data)
         .then((res) => {
-          const user = {
-            accessToken: res.data.accessToken,
-            userData: res.data.userData,
-          };
-          localStorage.setItem("user", JSON.stringify(user));
+          // const user = {
+          //   accessToken: res.data.accessToken,
+          //   userData: res.data.userData,
+          // };
+
+          // localStorage.setItem("user", JSON.stringify(user));
+
+          localStorage.setItem("accessToken", res.data.accessToken);
+          dispatch(login(res.data.userData));
+
           navigate("/");
         })
         .catch((error: any) => {
